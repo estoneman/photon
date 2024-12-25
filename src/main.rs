0c47a804bf0102fa85d1b95a16b69525a0d0bfb0
estoneman <elstoneman11@gmail.com>
2024-12-25 09:08:20 -0600

    separate mp3 file existence and fetching functionality

--- src/main.rs
+++ src/main.rs
@@ -84,38 +84,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
-    if !Path::new(format!("mp3/{}.mp3", youtube_id.clone()).as_str()).exists() {
-        let c = CNVClient {
-            client: reqwest::Client::new(),
-        };
-
-        let checkdb_res = c.check_database(youtube_id.to_string()).await?;
-
-        match match_response(checkdb_res) {
-            ResponseCheckDatabase::Exist(data) => {
-                eprintln!("info: file exists in cdn");
-                c.cdn_download(data.data.server_path.clone(), youtube_id.to_string())
-                    .await?;
-            }
-            ResponseCheckDatabase::NoExist(_) => {
-                eprintln!("info: file does not exist in cdn");
-                let title = c.cdn_fetch(youtube_url.to_string()).await?;
-
-                let server_path = c
-                    .srv_download(youtube_url.to_string(), title.clone())
-                    .await?;
-
-                c.cdn_insert(server_path.clone(), title.clone(), youtube_id.to_string())
-                    .await?;
-
-                c.cdn_download(server_path, youtube_id.to_string()).await?;
-            }
-            ResponseCheckDatabase::Unknown(unknown) => {
-                eprintln!("error: unknown response type: {:?}", unknown);
-                exit(1);
-            }
-        };
+    if Path::new(format!("mp3/{}.mp3", youtube_id.clone()).as_str()).exists() {
+        println!("the requested video has already been saved locally as mp3");
+        return Ok(());
     }
+
+    let c = CNVClient {
+        client: reqwest::Client::new(),
+    };
+
+    let checkdb_res = c.check_database(youtube_id.to_string()).await?;
+
+    match match_response(checkdb_res) {
+        ResponseCheckDatabase::Exist(data) => {
+            eprintln!("info: file exists in cdn");
+            c.cdn_download(data.data.server_path.clone(), youtube_id.to_string())
+                .await?;
+        }
+        ResponseCheckDatabase::NoExist(_) => {
+            eprintln!("info: file does not exist in cdn");
+            let title = c.cdn_fetch(youtube_url.to_string()).await?;
+
+            let server_path = c
+                .srv_download(youtube_url.to_string(), title.clone())
+                .await?;
+
+            c.cdn_insert(server_path.clone(), title.clone(), youtube_id.to_string())
+                .await?;
+
+            c.cdn_download(server_path, youtube_id.to_string()).await?;
+        }
+        ResponseCheckDatabase::Unknown(unknown) => {
+            eprintln!("error: unknown response type: {:?}", unknown);
+            exit(1);
+        }
+    };
 
     Ok(())
 }
