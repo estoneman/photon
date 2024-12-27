4f97d248e75ae3290b51b3d9c84d33e9a78a71e3
estoneman <elstoneman11@gmail.com>
2024-12-27 05:56:21 -0600

    mod rename and move convert functionality out

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@
 
 use clap::{command, Parser, Subcommand};
 use std::ops::RangeInclusive;
-use std::path::Path;
-use std::process::exit;
 use url::Url;
 
-mod client;
-use client::*;
+mod convert;
+use convert::*;
 
 /// Top-level command-line argument specification
 #[derive(Parser)]
@@ -36,87 +34,6 @@ enum Commands {
         #[arg(long, value_name = "BPM", value_parser = bpm_in_range)]
         bpm: Option<u8>,
     },
-}
-
-/// Converts a YouTube video to an MP3 file and downloads it.
-///
-/// # Arguments
-///
-/// * `youtube_url` - The URL of the YouTube video to convert.
-///
-/// # Returns
-///
-/// * `Ok(())` - If the MP3 file is downloaded successfully.
-/// * `Err` - If an error occurs during conversion or download.
-///
-/// # Example
-///
-/// ```rust
-/// use url::Url;
-///
-/// #[tokio::main]
-/// async fn main() {
-///     let youtube_url = Url::parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ").unwrap();
-///     if let Err(e) = convert(youtube_url).await {
-///         eprintln!("Error: {}", e);
-///     } else {
-///         println!("Download successful!");
-///     }
-/// }
-/// ```
-///
-/// # Notes
-///
-/// This function uses `cnvmp3.com` to perform the conversion.
-async fn download(youtube_url: Url) -> Result<(), Box<dyn std::error::Error>> {
-    assert_eq!(youtube_url.host_str(), Some("www.youtube.com"));
-
-    let youtube_id: String = match youtube_url.query_pairs().next() {
-        Some(v) => v.1.to_string(),
-        None => {
-            eprintln!(
-                "error: youtube url is not valid (doesn't include youtube id in query string)"
-            );
-            exit(1);
-        }
-    };
-
-    if Path::new(format!("mp3/{}.mp3", youtube_id.clone()).as_str()).exists() {
-        println!("the requested video has already been saved locally as mp3");
-    }
-
-    let c = CNVClient {
-        client: reqwest::Client::new(),
-    };
-
-    let checkdb_res = c.check_database(youtube_id.to_string()).await?;
-
-    match match_response(checkdb_res) {
-        ResponseCheckDatabase::Exist(data) => {
-            eprintln!("info: file exists in cdn");
-            c.cdn_download(data.data.server_path.clone(), youtube_id.to_string())
-                .await?;
-        }
-        ResponseCheckDatabase::NoExist(_) => {
-            eprintln!("info: file does not exist in cdn");
-            let title = c.cdn_fetch(youtube_url.to_string()).await?;
-
-            let server_path = c
-                .srv_download(youtube_url.to_string(), title.clone())
-                .await?;
-
-            c.cdn_insert(server_path.clone(), title.clone(), youtube_id.to_string())
-                .await?;
-
-            c.cdn_download(server_path, youtube_id.to_string()).await?;
-        }
-        ResponseCheckDatabase::Unknown(unknown) => {
-            eprintln!("error: unknown response type: {:?}", unknown);
-            exit(1);
-        }
-    };
-
-    Ok(())
 }
 
 const BPM_RANGE: RangeInclusive<u8> = 120..=140;
@@ -148,8 +65,7 @@ fn analysis(bpm: Option<u8>) {
     eprintln!("this code will not run, it is not finished");
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
@@ -158,13 +74,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Commands::Download {
             youtube_url,
             dest_type,
-        } => {
-            download(youtube_url.clone()).await?;
-        }
+        } => match download(youtube_url.clone()) {
+            Ok(_) => eprintln!("info: download complete"),
+            Err(e) => eprintln!("{:?}", e),
+        },
         Commands::Analysis { bpm } => {
             analysis(*bpm);
         }
     }
-
-    Ok(())
 }
